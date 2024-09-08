36604afebe72604887e7c13ab81c6a0441fb3cdf
zivmax <zivmax@foxmail.com>
2024-09-08 17:31:38 +0800

    commit

--- src/prob-3/planning.py
+++ src/prob-3/planning.py
@@ -45,7 +45,7 @@ def planning(
         for j in crops_id:
             for k in years:
                 for s in seasons_id:
-                    A_ijks[(i, j, k, s)].name = f"A_{i}_{j}_{k}_{s}"
+                    A_ijks[i, j, k, s].name = f"A_{i}_{j}_{k}_{s}"
 
     # A_{ijk} = \sum_s A^2_{ijks} + A^1_{ij(k-1)2} + A^1_{ijks}
     A_ijk: dict[tuple : pulp.LpVariable] = {}
@@ -54,7 +54,7 @@ def planning(
             for k in years:
                 if k == 2023:
                     continue
-                A_ijk[(i, j, k)] = pulp.lpSum([A_ijks[i, j, k, s] for s in seasons_id])
+                A_ijk[i, j, k] = pulp.lpSum([A_ijks[i, j, k, s] for s in seasons_id])
 
     # 2023 年的 A_{ijks} 已知
     for i in fields_id:
@@ -72,15 +72,13 @@ def planning(
                     else 0
                 )
 
-                A_ijks[(i, j, 2023, s)].lowBound = area
-                A_ijks[(i, j, 2023, s)].upBound = area
+                A_ijks[i, j, 2023, s].lowBound = area
+                A_ijks[i, j, 2023, s].upBound = area
 
     # 2023 年的 A_{ijk} 已知
     for i in fields_id:
         for j in crops_id:
-            A_ijk[(i, j, 2023)] = pulp.lpSum(
-                [A_ijks[i, j, 2023, s] for s in seasons_id]
-            )
+            A_ijk[i, j, 2023] = pulp.lpSum([A_ijks[i, j, 2023, s] for s in seasons_id])
 
     B_ijks = pulp.LpVariable.dicts(
         "B",
@@ -116,9 +114,9 @@ def planning(
     L = pulp.lpSum(
         [
             (
-                Y[(j, k)] * A_ijk[(i, j, k)] * P[(j, k)]
-                - C[(j, k)] * A_ijk[(i, j, k)]
-                - alpha * Z_ijk[(i, j, k)] * P[(j, k)]
+                Y[j, k] * A_ijk[i, j, k] * P[j, k]
+                - C[j, k] * A_ijk[i, j, k]
+                - alpha * Z_ijk[i, j, k] * P[j, k]
             )
             for i in fields_id
             for j in crops_id
@@ -128,12 +126,12 @@ def planning(
 
     # 设置目标函数
     prob += L
-    
+
     # 添加约束 Z_ijk >= Y_jk * A_ijk - S_jk
     for i in fields_id:
         for j in crops_id:
             for k in years:
-                prob += Z_ijk[(i, j, k)] >= Y[(j, k)] * A_ijk[(i, j, k)] - S[(j, k)]
+                prob += Z_ijk[i, j, k] >= Y[j, k] * A_ijk[i, j, k] - S[j, k]
     # 添加约束 Z_ijk >= 0 已经通过 LpVariable 的 lowBound=0 实现
 
     M1 = 10000  # 一个很大的数
@@ -141,19 +139,19 @@ def planning(
         for j in crops_id:
             for k in years:
                 for s in seasons_id:
-                    prob += A_ijks[(i, j, k, s)] <= B_ijks[(i, j, k, s)] * M1
+                    prob += A_ijks[i, j, k, s] <= B_ijks[i, j, k, s] * M1
 
     # 约束：一个地块上每种作物合起来的种植面积不超过相应地块的总面积
     for i in fields_id:
         for s in seasons_id:
             for k in years:
                 prob += (
-                    pulp.lpSum([A_ijks[(i, j, k, s)] for j in T_hat_i_s[(i, s)]])
+                    pulp.lpSum([A_ijks[i, j, k, s] for j in T_hat_i_s[i, s]])
                     <= A_star[i]
                 )
                 for j in crops_id:
-                    if j not in T_hat_i_s[(i, s)]:
-                        prob += A_ijks[(i, j, k, s)] == 0
+                    if j not in T_hat_i_s[i, s]:
+                        prob += A_ijks[i, j, k, s] == 0
 
     # 约束：不能重茬种植
     for i in fields_id:
@@ -163,11 +161,11 @@ def planning(
                     continue
 
                 # 如果作物 j 在第 i 块地的第 1 季度被种植
-                if j in T_hat_i_s[(i, 1)] and j not in T_hat_i_s[(i, 2)]:
-                    prob += B_ijks[(i, j, k, 1)] + B_ijks[(i, j, k + 1, 1)] <= 1
+                if j in T_hat_i_s[i, 1] and j not in T_hat_i_s[i, 2]:
+                    prob += B_ijks[i, j, k, 1] + B_ijks[i, j, k + 1, 1] <= 1
                 # 如果作物 j 在第 i 块地的第 1 和第 2 季度都被种植
-                elif j in T_hat_i_s[(i, 1)] and j in T_hat_i_s[(i, 2)]:
-                    prob += B_ijks[(i, j, k, 1)] + B_ijks[(i, j, k, 2)] <= 1
+                elif j in T_hat_i_s[i, 1] and j in T_hat_i_s[i, 2]:
+                    prob += B_ijks[i, j, k, 1] + B_ijks[i, j, k, 2] <= 1
 
     # 约束：每个地块（含大棚）的所有土地三年内至少种植一次豆类作物
     for i in fields_id:
@@ -177,7 +175,7 @@ def planning(
             prob += (
                 pulp.lpSum(
                     [
-                        B_ijks[(i, j, kk, s)]
+                        B_ijks[i, j, kk, s]
                         for kk in range(k, k + 3)
                         for s in seasons_id
                         for j in beans
@@ -190,7 +188,7 @@ def planning(
                 for s in seasons_id:
                     if j in T_hat_i_s[i, s]:
                         # 如果种植，则种植整个地块面积
-                        prob += A_ijks[(i, j, k, s)] == A_star[i] * B_ijks[i, j, k, s]
+                        prob += A_ijks[i, j, k, s] == A_star[i] * B_ijks[i, j, k, s]
 
     # 约束：第一季种了水稻的水浇地，第二季不能种 B 类蔬菜
     for i in fields_id:
@@ -198,27 +196,36 @@ def planning(
             if t_i[i] == "水浇地":
                 for j1 in grains_B:
                     for j2 in vege_B:
-                        prob += B_ijks[(i, j1, k, 1)] + B_ijks[(i, j2, k, 2)] <= 1
+                        prob += B_ijks[i, j1, k, 1] + B_ijks[i, j2, k, 2] <= 1
 
     # 约束：第一季种了 A 类蔬菜的水浇地，第二季的 B 类蔬菜不能混种在一个地块上
     for i in fields_id:
         for k in years:
             if t_i[i] == "水浇地":
                 for j2 in vege_B:
-                    prob += A_ijks[(i, j2, k, 2)] == A_star[i] * B_ijks[i, j2, k, 2]
+                    prob += A_ijks[i, j2, k, 2] == A_star[i] * B_ijks[i, j2, k, 2]
 
     M2 = 0.3
     # 约束：单块地的种植面积不能过小
     for i in fields_id:
         for k in years:
             for s in seasons_id:
-                for j in T_hat_i_s[(i, s)]:
-                    prob += (
-                        A_ijks[(i, j, k, s)] >= M2 * A_star[i] * B_ijks[(i, j, k, s)]
-                    )
-    
-    # 约束：单块地的种植面积不能过小
-    
+                for j in T_hat_i_s[i, s]:
+                    prob += A_ijks[i, j, k, s] >= M2 * A_star[i] * B_ijks[i, j, k, s]
+
+    # 约束：作物之间的互替性约束
+    for i in fields_id:
+        for k in years:
+            for s in seasons_id:
+                for j1 in crops_id:
+                    for j2 in crops_id:
+                        if j1 == j2:
+                            continue
+                        if j1 in T_hat_i_s[i, s] and j2 in T_hat_i_s[i, s]:
+                            prob += (
+                                A_ijks[i, j1, k, s] + R_ij[j1, j2] * A_ijks[i, j2, k, s]
+                                <= A_star[i]
+                            )
 
     # # 模型求解
     prob.solve()
